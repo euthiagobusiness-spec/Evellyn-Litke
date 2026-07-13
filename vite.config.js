3bd6854a349845d9b940bcd4977ca48104e322fe
Thiago Carvalho <euthiagobusiness@gmail.com>
2026-07-13 03:26:28 -0400

    Ajusta selo carrosseis e paginas legais

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ module.exports = defineConfig({
         captura: path.resolve(__dirname, "index.html"),
         upsell: path.resolve(__dirname, "pagina-vendas.html"),
         obrigado: path.resolve(__dirname, "obrigado.html"),
+        privacidade: path.resolve(__dirname, "politica-de-privacidade.html"),
+        termos: path.resolve(__dirname, "termos-de-uso.html"),
       },
     },
   },
